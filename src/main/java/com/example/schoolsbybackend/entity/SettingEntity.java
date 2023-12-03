f2ae7c28d0a7e5e464f0b1546d0b6fb85d11826c
MaximChirich <maximkanavalcuk@gmail.com>
2023-12-03 20:29:57 +0300

    get and set SettingEntity

--- src/main/java/com/example/schoolsbybackend/entity/SettingEntity.java
+++ src/main/java/com/example/schoolsbybackend/entity/SettingEntity.java
@@ -1,9 +1,9 @@
 package com.example.schoolsbybackend.entity;
 
 import jakarta.annotation.Nullable;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 public class SettingEntity {
     @Id
@@ -16,4 +16,57 @@ public class SettingEntity {
     private Boolean notificationsOn;
     @Nullable
     private Boolean pushNotificationsOn;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+
+    // Дура почему-то ругается на Long при добавлении @Entity к классу
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public Boolean getDarkTheme() {
+        return darkTheme;
+    }
+
+    public void setDarkTheme(Boolean darkTheme) {
+        this.darkTheme = darkTheme;
+    }
+
+    public Long getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(Long fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public Boolean getNotificationsOn() {
+        return notificationsOn;
+    }
+
+    public void setNotificationsOn(Boolean notificationsOn) {
+        this.notificationsOn = notificationsOn;
+    }
+
+    @Nullable
+    public Boolean getPushNotificationsOn() {
+        return pushNotificationsOn;
+    }
+
+    public void setPushNotificationsOn(@Nullable Boolean pushNotificationsOn) {
+        this.pushNotificationsOn = pushNotificationsOn;
+    }
 }
