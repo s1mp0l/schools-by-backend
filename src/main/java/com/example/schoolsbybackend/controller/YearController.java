e9bbd718121bcf215c9919b610362c551fcd791e
MaximChirich <maximkanavalcuk@gmail.com>
2023-12-07 21:29:01 +0300

    Year

---
+++ src/main/java/com/example/schoolsbybackend/controller/YearController.java
@@ -0,0 +1,43 @@
+package com.example.schoolsbybackend.controller;
+
+import com.example.schoolsbybackend.service.YearService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/years")
+public class YearController {
+    @Autowired
+    private YearService yearService;
+
+
+
+    @GetMapping
+    public ResponseEntity getAllYears() {
+        try {
+            return ResponseEntity.ok(yearService.getAllYears());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOneYear(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(yearService.getById(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteYear(@PathVariable Long id) {
+        try {
+            yearService.delete(id);
+            return ResponseEntity.ok("Год успешно удален!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/example/schoolsbybackend/entity/YearEntity.java
@@ -0,0 +1,62 @@
+package com.example.schoolsbybackend.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class YearEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonFormat(pattern="dd-MM-yyyy")
+    private LocalDate start_date;
+
+    @JsonFormat(pattern="HH:mm:ss")
+    private LocalDate end_date;
+
+    @Nullable
+    private Long year_mark;
+
+    public YearEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDate start_date) {
+        this.start_date = start_date;
+    }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDate end_date) {
+        this.end_date = end_date;
+    }
+
+    @Nullable
+    public Long getYear_mark() {
+        return year_mark;
+    }
+
+    public void setYear_mark(@Nullable Long year_mark) {
+        this.year_mark = year_mark;
+    }
+}
